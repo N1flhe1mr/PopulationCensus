2c1b8825a86529d6b3872eb51db4317c105911e3
Dmitriy_Korchagin <793070407232@ya.ru>
2023-02-05 21:51:53 +0300

    main fix

--- src/Main.java
+++ src/Main.java
@@ -23,21 +23,18 @@ public class Main {
         System.out.println("Совершеннолетие население: " + adultPersons + " человек.");
 
         var conscriptMans = persons.stream()
-                .filter(person -> person.getSex().equals(Sex.MAN) &&
-                        person.getAge() >= 18 && person.getAge() <= 27)
+                .filter(person -> person.getSex().equals(Sex.MAN))
+                .filter(person -> person.getAge() <= 27)
+                .filter(person -> person.getAge() >= 18)
                 .map(Person::getFamily)
                 .collect(Collectors.toList());
         System.out.println("Мужчины призывного возраста: " + conscriptMans);
 
         var workingPersons = persons.stream()
-                .filter(person ->
-                        person.getSex().equals(Sex.MAN) &&
-                        person.getEducation().equals(Education.HIGHER) &&
-                        person.getAge() >= 18 && person.getAge() <= 70 ||
-                        person.getSex().equals(Sex.WOMAN) &&
-                        person.getEducation().equals(Education.HIGHER) &&
-                        person.getAge() >= 18 && person.getAge() <= 65
-                )
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getSex().equals(Sex.MAN) && person.getAge() <= 70 ||
+                        person.getSex().equals(Sex.WOMAN) && person.getAge() <= 65)
                 .sorted(Comparator.comparing(Person::getFamily))
                 .map(Person::getFamily)
                 .collect(Collectors.toList());
